9f2f398fb9c4188133f017d56a2b9b8eca427aa5
Siu <siuc@protonmail.com>
2022-01-31 15:25:46 -0500

    adjust the padding

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -3,7 +3,11 @@ import "./Board.css";
 import BoardList from "./BoardList";
 
 const Board = () => {
-  return <BoardList />;
+  const lst = [];
+  for (let i = 0; i < 10; i++) {
+    lst.push(<BoardList key={i} />);
+  }
+  return lst;
 }
 
 export default Board;
