0b16392033393a95f61973a7dc79844492b79739
Siu <siuc@protonmail.com>
2022-01-31 16:17:42 -0500

    add ShipListItem

--- src/Components/ShipList.jsx
+++ src/Components/ShipList.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./ShipList.css";
+import ShipListItem from "./ShipListItem";
 
 const ShipList = () => {
-  return <div className="ship-list">hello</div>;
+  const lst = [];
+  for (let i = 0; i < 5; i++) {
+    lst.push(<ShipListItem />);
+  }
+  return <div className="ship-list">{lst}</div>;
 };
 
 export default ShipList;
---
+++ src/Components/ShipListItem.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ShipList.css";
+
+const ShipListItem = () => {
+  return <div>hello</div>;
+};
+
+export default ShipListItem;
