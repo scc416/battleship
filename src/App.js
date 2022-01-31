b9e15caa80489cb8d1d58099c73f7cb352eed833
Siu <siuc@protonmail.com>
2022-01-30 20:55:08 -0500

    format

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import io from 'socket.io-client';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import io from "socket.io-client";
 
-const socket = io('localhost:3001');
+const socket = io("localhost:3001");
 
-function App() {
+const App = () => {
   const [isConnected, setIsConnected] = useState(socket.connected);
   const [lastMessage, setLastMessage] = useState(null);
 
   useEffect(() => {
-    socket.on('connect', () => {
+    socket.on("connect", () => {
       setIsConnected(true);
     });
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
       setIsConnected(false);
     });
-    socket.on('message', data => {
+    socket.on("message", (data) => {
       setLastMessage(data);
     });
     return () => {
-      socket.off('connect');
-      socket.off('disconnect');
-      socket.off('message');
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("message");
     };
   });
 
   const sendMessage = () => {
-    socket.emit('hello!');
-  }
+    socket.emit("hello!");
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>Connected: { '' + isConnected }</p>
-        <p>Last message: { lastMessage || '-' }</p>
-        <button onClick={ sendMessage }>Say hello!</button>
+        <p>Connected: {"" + isConnected}</p>
+        <p>Last message: {lastMessage || "-"}</p>
+        <button onClick={sendMessage}>Say hello!</button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
