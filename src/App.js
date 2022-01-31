e2858647b3408c3236154fa24d2d5368555ebf08
Siu <siuc@protonmail.com>
2022-01-30 21:19:23 -0500

    add board component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import io from "socket.io-client";
+import Board from "./Components/Board";
 
 const socket = io("localhost:3001");
 
 const App = () => {
-  const [isConnected, setIsConnected] = useState(socket.connected);
-  const [lastMessage, setLastMessage] = useState(null);
+  // const [isConnected, setIsConnected] = useState(socket.connected);
+  // const [lastMessage, setLastMessage] = useState(null);
 
-  useEffect(() => {
-    socket.on("connect", () => {
-      setIsConnected(true);
-    });
-    socket.on("disconnect", () => {
-      setIsConnected(false);
-    });
-    socket.on("message", (data) => {
-      setLastMessage(data);
-    });
-    return () => {
-      socket.off("connect");
-      socket.off("disconnect");
-      socket.off("message");
-    };
-  });
+  // useEffect(() => {
+  // socket.on("connect", () => {
+  //   setIsConnected(true);
+  // });
+  // socket.on("disconnect", () => {
+  //   setIsConnected(false);
+  // });
+  // socket.on("message", (data) => {
+  //   setLastMessage(data);
+  // });
+  //   return () => {
+  //     socket.off("connect");
+  //     socket.off("disconnect");
+  //     socket.off("message");
+  //   };
+  // });
 
-  const sendMessage = () => {
-    socket.emit("hello!");
-  };
+  // const sendMessage = () => {
+  //   socket.emit("hello!");
+  // };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Connected: {"" + isConnected}</p>
-        <p>Last message: {lastMessage || "-"}</p>
-        <button onClick={sendMessage}>Say hello!</button>
-      </header>
-    </div>
+    <>
+      <h1>Battleship</h1>
+      <div className="boards">
+        <div>
+          Your Board
+          <Board />
+        </div>
+        <div>
+          Opponent's Board
+          <Board />
+        </div>
+      </div>
+      <div>HELLO</div>
+    </>
   );
 };
 
