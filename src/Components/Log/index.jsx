d45cce9738438257aae4be5a96205183e0851c22
Siu <siuc@protonmail.com>
2022-01-31 17:51:05 -0500

    scroll to bottom (log list)

--- src/Components/Log/index.jsx
+++ src/Components/Log/index.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./Log.css";
 import LogListItem from "./LogListItem";
 
-const LogList = () => {
+const LogList = ({ logs }) => {
+  const log = useRef(null);
+
+  useEffect(() => {
+    if (log && log.current) {
+      log.current.scrollTop = log.current.scrollHeight;
+    }
+  }, [log, logs]);
+
   return (
-    <div className="logs">
+    <div className="logs" ref={log}>
       <LogListItem />
       <LogListItem />
       <LogListItem />
