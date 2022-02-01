5c9534f94d0b62ccf307b6e0fb20ba887a768771
Siu <siuc@protonmail.com>
2022-02-01 17:44:10 -0500

    pair up the socket

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
+const cli = require("nodemon/lib/cli");
+
 const io = require("socket.io")({
   cors: {
     origin: ["http://localhost:3000"],
   },
 });
 
-const clients = [];
+const clients = {};
+const getKeys = () => Object.keys(clients);
 
 io.on("connection", (socket) => {
-  clients.push(socket.id);
+  const keys = getKeys();
+  for (let i = 0; i < keys.length; i++) {
+    const otherSocket = clients[keys[i]];
+    if (!otherSocket) {
+      clients[keys[i]] = socket.id;
+      clients[socket.id] = keys[i];
+      i = keys.length;
+    }
+  }
+  const newKey = getKeys();
+  if (!newKey.includes(socket.id)) {
+    clients[socket.id] = null;
+  }
+
   console.log("clients", clients);
 
   socket.on("disconnect", () => {
     console.log(`disconnect: ${socket.id}`);
-    const index = clients.indexOf(socket.id);
-    if (index > -1) clients.splice(index, 1);
+    const otherSocket = clients[socket.id];
+    if (otherSocket) clients[otherSocket] = null;
+    delete clients[socket.id];
     console.log("clients", clients);
   });
 });
