4efc0a36d5af6f8290ca1dc1ea19da3516c870f0
Siu <siuc@protonmail.com>
2022-02-01 18:00:55 -0500

    useState for the different states

--- src/hooks/useGame.js
+++ src/hooks/useGame.js
@@ -5,12 +5,40 @@ const socket = io("localhost:3001");
 
 const useGame = () => {
   const [state, setState] = useState(0);
-  // const [messages, setMessages], myShips, opponentShips]
+  const [messages, setMessages] = useState([]);
+  const [myShips, setMyShips] = useState([]);
+  const [opponentShips, setOpponentShips] = useState([]);
+  // , myShips, opponentShips]
 
   useEffect(() => {
     socket.on("connect", () => {
       console.log(socket);
     });
+
+    setMessages([
+      { time: "1", content: "hello" },
+      { time: "2", content: "hello" },
+      { time: "3", content: "hello" },
+      { time: "4", content: "hello" },
+      { time: "5", content: "hello" },
+      { time: "6", content: "hello" },
+    ]);
+
+    setMyShips([
+      { name: "Carrier", destroyed: true },
+      { name: "Battleship", destroyed: true },
+      { name: "Cruiser", destroyed: false },
+      { name: "Submarine", destroyed: false },
+      { name: "Destroyer", destroyed: true },
+    ]);
+
+    setOpponentShips([
+      { name: "Carrier", destroyed: true },
+      { name: "Battleship", destroyed: false },
+      { name: "Cruiser", destroyed: true },
+      { name: "Submarine", destroyed: false },
+      { name: "Destroyer", destroyed: false },
+    ]);
 
     // socket.on("disconnect", () => {
     //   setIsConnected(false);
@@ -20,36 +48,10 @@ const useGame = () => {
     // });
     return () => {
       socket.off("connect");
-      socket.emit("disconnect");
       // socket.off("disconnect");
       // socket.off("message");
     };
-  });
-
-  const messages = [
-    { time: "1", content: "hello" },
-    { time: "2", content: "hello" },
-    { time: "3", content: "hello" },
-    { time: "4", content: "hello" },
-    { time: "5", content: "hello" },
-    { time: "6", content: "hello" },
-  ];
-
-  const myShips = [
-    { name: "Carrier", destroyed: true },
-    { name: "Battleship", destroyed: true },
-    { name: "Cruiser", destroyed: false },
-    { name: "Submarine", destroyed: false },
-    { name: "Destroyer", destroyed: true },
-  ];
-
-  const opponentShips = [
-    { name: "Carrier", destroyed: true },
-    { name: "Battleship", destroyed: false },
-    { name: "Cruiser", destroyed: true },
-    { name: "Submarine", destroyed: false },
-    { name: "Destroyer", destroyed: false },
-  ];
+  }, []);
 
   // const sendMessage = () => {
   //   socket.emit("hello!");
