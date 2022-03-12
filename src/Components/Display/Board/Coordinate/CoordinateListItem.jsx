472c2259673a3c67d283f621d320bc38a349374b
Siu <siuc@protonmail.com>
2022-03-12 18:58:55 -0500

    update icon for selected tiles: anchor

--- src/Components/Display/Board/Coordinate/CoordinateListItem.jsx
+++ src/Components/Display/Board/Coordinate/CoordinateListItem.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAnchor } from "@fortawesome/free-solid-svg-icons";
 
 const CoordinateListItem = ({ clickHandler, selected }) => {
   if (selected) console.log("SELECTED");
   return (
     <div className="square" onClick={clickHandler}>
-      {selected && <FontAwesomeIcon icon="fa-regular fa-anchor" />}
+      {selected && <FontAwesomeIcon icon={faAnchor} />}
     </div>
   );
 };
