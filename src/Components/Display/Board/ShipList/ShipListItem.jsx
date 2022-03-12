f8e593b6b6249757e9f2a86bbd7bd818e828702b
Siu <siuc@protonmail.com>
2022-03-12 16:00:33 -0500

    remove destroyed from myShips and opponentShips, replace by a function working out if any tiles are not hit

--- src/Components/Display/Board/ShipList/ShipListItem.jsx
+++ src/Components/Display/Board/ShipList/ShipListItem.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./ShipList.css";
 
-const ShipListItem = ({ ship }) => {
-  const style = ship.destroyed ? { textDecoration: "line-through" } : {};
-  return <div style={style}>{ship.name}</div>;
+const ShipListItem = ({ ship: { name, positions } }) => {
+  const allDestroyed = positions.reduce(({ hit }, destroyed) => {
+    if (!hit) return false;
+  }, true);
+
+  const style = allDestroyed ? { textDecoration: "line-through" } : {};
+  return <div style={style}>{name}</div>;
 };
 
 export default ShipListItem;
--- src/constants.js
+++ src/constants.js
@@ -4,28 +4,32 @@ export const NEW_OPPONENT = "NEW_OPPONENT";
 export const NEW_MESSAGE = "NEW_MESSAGE";
 export const RESET = "RESET";
 
-export const INITIAL_MSG_NO_OPPONENT = "There is no player in the room. Waiting for another player...";
-export const INITIAL_MSG_HAVE_OPPONENT = "Another player is already in the room. The game is on!";
-export const MSG_HAVE_OPPONENT = "Another player has entered the game. The game is on!";
-export const MSG_NO_OPPONENT = "The other player left. Waiting for another player...";
+export const INITIAL_MSG_NO_OPPONENT =
+  "There is no player in the room. Waiting for another player...";
+export const INITIAL_MSG_HAVE_OPPONENT =
+  "Another player is already in the room. The game is on!";
+export const MSG_HAVE_OPPONENT =
+  "Another player has entered the game. The game is on!";
+export const MSG_NO_OPPONENT =
+  "The other player left. Waiting for another player...";
 
 export const initialState = () => {
   return {
     gameState: 0,
     messages: [{ time: getCurrentTime(), message: "Welcome to Battleship!" }],
     myShips: [
-      { name: "Carrier", destroyed: true, position: [] },
-      { name: "Battleship", destroyed: true, position: [] },
-      { name: "Cruiser", destroyed: false, position: [] },
-      { name: "Submarine", destroyed: false, position: [] },
-      { name: "Destroyer", destroyed: true, position: [] },
+      { name: "Carrier", destroyed: true, positions: [{ hit: false }] },
+      { name: "Battleship", destroyed: true, positions: [] },
+      { name: "Cruiser", destroyed: false, positions: [] },
+      { name: "Submarine", destroyed: false, positions: [] },
+      { name: "Destroyer", destroyed: true, positions: [] },
     ],
     opponentShips: [
-      { name: "Carrier", destroyed: true, position: [] },
-      { name: "Battleship", destroyed: false, position: [] },
-      { name: "Cruiser", destroyed: true, position: [] },
-      { name: "Submarine", destroyed: false, position: [] },
-      { name: "Destroyer", destroyed: false, position: [] },
+      { name: "Carrier", destroyed: true, positions: [] },
+      { name: "Battleship", destroyed: false, positions: [] },
+      { name: "Cruiser", destroyed: true, positions: [] },
+      { name: "Submarine", destroyed: false, positions: [] },
+      { name: "Destroyer", destroyed: false, positions: [] },
     ],
     opponent: undefined,
     gotInitialOpponent: false,
