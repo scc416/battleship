9e191454a2137942cd5d8a650e7f044645f74026
Siu <siuc@protonmail.com>
2022-03-12 23:04:52 -0500

    show strike on ship list according to hit ship

--- src/Components/Display/Board/ShipList/ShipListItem.jsx
+++ src/Components/Display/Board/ShipList/ShipListItem.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./ShipList.css";
+import { checkIfSameCoordinate } from "../../../../helpers";
 
-const ShipListItem = ({ ship: { name, coordinates } }) => {
-  const allDestroyed = true;
+const ShipListItem = ({ ship: { name, coordinates }, shot }) => {
+  const allDestroyed = coordinates.reduce((shipCoordinate, destroyed) => {
+    for (const coordinate of shot) {
+      const hit = checkIfSameCoordinate(shipCoordinate, coordinate);
+      if (hit) return destroyed;
+    }
+    return false;
+  }, true);
 
   const style = allDestroyed ? { textDecoration: "line-through" } : {};
   return <div style={style}>{name}</div>;
