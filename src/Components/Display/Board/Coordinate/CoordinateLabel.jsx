b841db852001475d49c778d6842ad688c31cf577
Siu <siuc@protonmail.com>
2022-03-14 15:40:55 -0400

    refactoring by breaking into smaller component

---
+++ src/Components/Display/Board/Coordinate/CoordinateLabel.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const CoordinateLabelList = ({ isRow }) => {
+  if (isRow) {
+    return (
+      <div>
+        <div className="coordinate-label-row">1</div>
+        <div className="coordinate-label-row">2</div>
+        <div className="coordinate-label-row">3</div>
+        <div className="coordinate-label-row">4</div>
+        <div className="coordinate-label-row">5</div>
+        <div className="coordinate-label-row">6</div>
+        <div className="coordinate-label-row">7</div>
+        <div className="coordinate-label-row">8</div>
+        <div className="coordinate-label-row">9</div>
+        <div className="coordinate-label-row">10</div>
+      </div>
+    );
+  }
+  return (
+    <div className="row-column column">
+      <div className="coordinate-label-column">A</div>
+      <div className="coordinate-label-column">B</div>
+      <div className="coordinate-label-column">C</div>
+      <div className="coordinate-label-column">D</div>
+      <div className="coordinate-label-column">E</div>
+      <div className="coordinate-label-column">F</div>
+      <div className="coordinate-label-column">G</div>
+      <div className="coordinate-label-column">H</div>
+      <div className="coordinate-label-column">I</div>
+      <div className="coordinate-label-column">J</div>
+    </div>
+  );
+};
+
+export default CoordinateLabelList;
--- src/Components/Display/Board/Coordinate/index.jsx
+++ src/Components/Display/Board/Coordinate/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Coordinate.css";
 import CoordinateList from "./CoordinateList";
+import CoordinateLabelList from "./CoordinateLabelList";
 
 const Coordinate = ({ placedShips, clickTile, chosenTiles, shot, myBoard }) => {
   const lst = [];
@@ -24,32 +25,10 @@ const Coordinate = ({ placedShips, clickTile, chosenTiles, shot, myBoard }) => {
     <>
       <div className="row-column">
         <div className="coordinate-space"></div>
-        <div className="row-column column">
-          <div className="coordinate-label-column">A</div>
-          <div className="coordinate-label-column">B</div>
-          <div className="coordinate-label-column">C</div>
-          <div className="coordinate-label-column">D</div>
-          <div className="coordinate-label-column">E</div>
-          <div className="coordinate-label-column">F</div>
-          <div className="coordinate-label-column">G</div>
-          <div className="coordinate-label-column">H</div>
-          <div className="coordinate-label-column">I</div>
-          <div className="coordinate-label-column">J</div>
-        </div>
+        <CoordinateLabelList />
       </div>
       <div className="row-column">
-        <div>
-          <div className="coordinate-label-row">1</div>
-          <div className="coordinate-label-row">2</div>
-          <div className="coordinate-label-row">3</div>
-          <div className="coordinate-label-row">4</div>
-          <div className="coordinate-label-row">5</div>
-          <div className="coordinate-label-row">6</div>
-          <div className="coordinate-label-row">7</div>
-          <div className="coordinate-label-row">8</div>
-          <div className="coordinate-label-row">9</div>
-          <div className="coordinate-label-row">10</div>
-        </div>
+        <CoordinateLabelList isRow={true} />
         <div className="coordinate">{lst}</div>
       </div>
     </>
