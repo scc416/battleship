2970d5742565311c844743c820b5198a5eb80619
Siu <siuc@protonmail.com>
2022-03-14 15:18:19 -0400

    adjust size of the label of row/column

--- src/Components/Display/Board/Coordinate/index.jsx
+++ src/Components/Display/Board/Coordinate/index.jsx
@@ -23,33 +23,33 @@ const Coordinate = ({ placedShips, clickTile, chosenTiles, shot, myBoard }) => {
   return (
     <>
       <div style={{ display: "flex" }}>
-        <div className="coordinate-label"></div>
-        <div style={{ display: "flex" }}>
-          <div className="coordinate-label">A</div>
-          <div className="coordinate-label">B</div>
-          <div className="coordinate-label">C</div>
-          <div className="coordinate-label">D</div>
-          <div className="coordinate-label">E</div>
-          <div className="coordinate-label">F</div>
-          <div className="coordinate-label">G</div>
-          <div className="coordinate-label">H</div>
-          <div className="coordinate-label">I</div>
-          <div className="coordinate-label">J</div>
+        <div className="coordinate-space"></div>
+        <div style={{ display: "flex", alignItems: "flex-end" }}>
+          <div className="coordinate-label-column">A</div>
+          <div className="coordinate-label-column">B</div>
+          <div className="coordinate-label-column">C</div>
+          <div className="coordinate-label-column">D</div>
+          <div className="coordinate-label-column">E</div>
+          <div className="coordinate-label-column">F</div>
+          <div className="coordinate-label-column">G</div>
+          <div className="coordinate-label-column">H</div>
+          <div className="coordinate-label-column">I</div>
+          <div className="coordinate-label-column">J</div>
         </div>
       </div>
       <div style={{ display: "flex" }}>
         <div>
           {" "}
-          <div className="coordinate-label">1</div>
-          <div className="coordinate-label">2</div>
-          <div className="coordinate-label">3</div>
-          <div className="coordinate-label">4</div>
-          <div className="coordinate-label">5</div>
-          <div className="coordinate-label">6</div>
-          <div className="coordinate-label">7</div>
-          <div className="coordinate-label">8</div>
-          <div className="coordinate-label">9</div>
-          <div className="coordinate-label">10</div>
+          <div className="coordinate-label-row">1</div>
+          <div className="coordinate-label-row">2</div>
+          <div className="coordinate-label-row">3</div>
+          <div className="coordinate-label-row">4</div>
+          <div className="coordinate-label-row">5</div>
+          <div className="coordinate-label-row">6</div>
+          <div className="coordinate-label-row">7</div>
+          <div className="coordinate-label-row">8</div>
+          <div className="coordinate-label-row">9</div>
+          <div className="coordinate-label-row">10</div>
         </div>
         <div className="coordinate">{lst}</div>
       </div>
