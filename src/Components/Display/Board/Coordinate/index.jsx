b10313bf513bab8626e3f90878b7ae1c7a098c96
Siu <siuc@protonmail.com>
2022-03-14 15:12:05 -0400

    rename classname from index to coordinate-label

--- src/Components/Display/Board/Coordinate/index.jsx
+++ src/Components/Display/Board/Coordinate/index.jsx
@@ -23,33 +23,33 @@ const Coordinate = ({ placedShips, clickTile, chosenTiles, shot, myBoard }) => {
   return (
     <>
       <div style={{ display: "flex" }}>
-        <div className="index"></div>
+        <div className="coordinate-label"></div>
         <div style={{ display: "flex" }}>
-          <div className="index">A</div>
-          <div className="index">B</div>
-          <div className="index">C</div>
-          <div className="index">D</div>
-          <div className="index">E</div>
-          <div className="index">F</div>
-          <div className="index">G</div>
-          <div className="index">H</div>
-          <div className="index">I</div>
-          <div className="index">J</div>
+          <div className="coordinate-label">A</div>
+          <div className="coordinate-label">B</div>
+          <div className="coordinate-label">C</div>
+          <div className="coordinate-label">D</div>
+          <div className="coordinate-label">E</div>
+          <div className="coordinate-label">F</div>
+          <div className="coordinate-label">G</div>
+          <div className="coordinate-label">H</div>
+          <div className="coordinate-label">I</div>
+          <div className="coordinate-label">J</div>
         </div>
       </div>
       <div style={{ display: "flex" }}>
         <div>
           {" "}
-          <div className="index">1</div>
-          <div className="index">2</div>
-          <div className="index">3</div>
-          <div className="index">4</div>
-          <div className="index">5</div>
-          <div className="index">6</div>
-          <div className="index">7</div>
-          <div className="index">8</div>
-          <div className="index">9</div>
-          <div className="index">10</div>
+          <div className="coordinate-label">1</div>
+          <div className="coordinate-label">2</div>
+          <div className="coordinate-label">3</div>
+          <div className="coordinate-label">4</div>
+          <div className="coordinate-label">5</div>
+          <div className="coordinate-label">6</div>
+          <div className="coordinate-label">7</div>
+          <div className="coordinate-label">8</div>
+          <div className="coordinate-label">9</div>
+          <div className="coordinate-label">10</div>
         </div>
         <div className="coordinate">{lst}</div>
       </div>
