0dc2ea6e5b28c75c55c60330a7d5010c18741f6f
Siu <siuc@protonmail.com>
2022-03-14 20:00:16 -0400

    refactor: hard code list into for loop list

--- src/Components/Display/Board/Coordinate/CoordinateLabelList.jsx
+++ src/Components/Display/Board/Coordinate/CoordinateLabelList.jsx
@@ -1,36 +1,18 @@
 import React from "react";
+import { columnLabel } from "../../../../constants";
 
 const CoordinateLabelList = ({ isRow }) => {
-  if (isRow) {
-    return (
-      <div>
-        <div className="coordinate-label-row">1</div>
-        <div className="coordinate-label-row">2</div>
-        <div className="coordinate-label-row">3</div>
-        <div className="coordinate-label-row">4</div>
-        <div className="coordinate-label-row">5</div>
-        <div className="coordinate-label-row">6</div>
-        <div className="coordinate-label-row">7</div>
-        <div className="coordinate-label-row">8</div>
-        <div className="coordinate-label-row">9</div>
-        <div className="coordinate-label-row">10</div>
-      </div>
-    );
+  const [parentDivClassName, coordinateClassName] = isRow
+    ? ["", "coordinate-label-row"]
+    : ["row-column column", "coordinate-label-column"];
+
+  const lst = [];
+
+  for (let i = 0; i < 10; i++) {
+    const label = isRow ? i + 1 : columnLabel[i];
+    lst.push(<div className={coordinateClassName}>{label}</div>);
   }
-  return (
-    <div className="row-column column">
-      <div className="coordinate-label-column">A</div>
-      <div className="coordinate-label-column">B</div>
-      <div className="coordinate-label-column">C</div>
-      <div className="coordinate-label-column">D</div>
-      <div className="coordinate-label-column">E</div>
-      <div className="coordinate-label-column">F</div>
-      <div className="coordinate-label-column">G</div>
-      <div className="coordinate-label-column">H</div>
-      <div className="coordinate-label-column">I</div>
-      <div className="coordinate-label-column">J</div>
-    </div>
-  );
+  return <div className={parentDivClassName}>{lst}</div>;
 };
 
 export default CoordinateLabelList;
